bd6c1a789e56f799fd1997d49919d5b707b18ca2
Raghul <raghulS2004>
2025-06-21 22:36:11 +0530

    Initial commit - MERN weather app

--- front/src/components/Major.jsx
+++ front/src/components/Major.jsx
@@ -4,26 +4,31 @@ import LastSection from "./LastSection";
 import Sidebar from "./Sidebar";
 import './css/major.css';
 
-function Major({ weatherData, forecastData}) {
-    const picUrl = `http://openweathermap.org/img/wn/${weatherData.weather?.[0].icon}@4x.png`;
-    return (
-        <div>
-            <div className="main-container">
-                <div className="lft">
-                    <h1>{weatherData?.name || "City Name"}</h1>
-                    <p>{(weatherData.weather?.[0].description || "NA").toUpperCase()}</p>
-                    <p>{weatherData?.coord?.lon},{weatherData?.coord?.lat}</p>
-                    <h1 className="degree">{Math.round(weatherData?.main?.temp) || "N/A"}°</h1>
-                </div>
-                <div className="rgt">
-                    <img  src={picUrl} alt="Weather Image" />
-                </div>
-            </div>
-            <MiddleSection forecastData={forecastData} />
-            <LastSection forecastData={forecastData} />
+function Major({ weatherData, forecastData }) {
+  const iconCode = weatherData?.weather?.[0]?.icon;
+  const picUrl = iconCode ? `https://openweathermap.org/img/wn/${iconCode}@4x.png` : "";
 
+  return (
+    <div>
+      <div className="main-container">
+        <div className="lft">
+          <h1>{weatherData?.name || "City Name"}</h1>
+          <p>{(weatherData.weather?.[0]?.description || "NA").toUpperCase()}</p>
+          <p>{weatherData?.coord?.lon},{weatherData?.coord?.lat}</p>
+          <h1 className="degree">{weatherData?.main?.temp !== undefined ? Math.round(weatherData.main.temp) : "N/A"}°</h1>
         </div>
-    );
+        <div className="rgt">
+          {picUrl ? (
+            <img src={picUrl} alt="Weather Image" />
+          ) : (
+            <p>Loading icon...</p>
+          )}
+        </div>
+      </div>
+      <MiddleSection forecastData={forecastData} />
+      <LastSection forecastData={forecastData} />
+    </div>
+  );
 }
 
 export default Major;
