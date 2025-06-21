18620a16317ca4c482db99b4e62377cdec61f097
Raghul <raghulS2004>
2025-06-21 21:19:05 +0530

    Initial commit - MERN weather app

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ router.use(cors({
     credentials: true
 }));
 router.post('/', async (req, res) => {
-    const apiKey = "73df2575dda41a3626e4ee7a5d7e9e4e";
+    const apiKey = "a4883c990c6327b02edd2edb3a12a544";
     const city = req.query.city;
     console.log(city);
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
@@ -21,7 +21,7 @@ router.post('/', async (req, res) => {
 });
 
 router.post('/forecast', async (req,res)=>{
-    const apikey1="755b0c53c74449b5a0c82746242608"; 
+    const apikey1="d7721ac41b854df2a3c151524252106"; 
     const city=req.query.city;
     console.log(city);
     const url="http://api.weatherapi.com/v1/forecast.json?key=755b0c53c74449b5a0c82746242608&aqi=yes&days=7&q="+city;
@@ -31,7 +31,7 @@ router.post('/forecast', async (req,res)=>{
     res.send(data)
 });
 router.post('/dayforecast', async (req,res)=>{
-    const apikey2="c3d594028bf14690befeefa4b91972cf"; 
+    const apikey2="b8260559b7a946fd96edc80e9d0651bc"; 
     const city=req.query.city;
     console.log(city);
     const url="https://api.weatherbit.io/v2.0/forecast/daily?key=c3d594028bf14690befeefa4b91972cf&city="+city;
